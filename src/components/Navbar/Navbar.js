8b8ad7b0b13e75581e4749716707c923b6b1e25c
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-05-20 12:17:40 -0700

    fixing navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,28 +4,29 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import "./Navbar.css"
 
 function Navtabs() {
-    return (
-        <Navbar className="nav-tabs-lg" expand="lg" variant="light">
-          <Container>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="navbar">
-                <Nav.Link as={Link} to="/" exact={true}>
-                  Home1
-                </Nav.Link>
-                <Nav.Link as={Link} to="/" exact={true}>
-                  Home2
-                </Nav.Link>
-                <Nav.Link as={Link} to="/" exact={true}>
-                  Home3
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      );
-    }
+  return (
+    <Navbar className="nav-tabs-lg navbar" expand="lg" variant="dark">
+      <Container className="navbar-container">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="menu-icon" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="nav-menu">
+            <Nav.Link as={Link} to="/" exact={true} className="nav-item">
+              <span className="nav-links">Home</span>
+            </Nav.Link>
+            <Nav.Link as={Link} to="/" exact={true} className="nav-item">
+              <span className="nav-links">Home</span>
+            </Nav.Link>
+            <Nav.Link as={Link} to="/" exact={true} className="nav-item">
+              <span className="nav-links">Home</span>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
-export default Navtabs
+export default Navtabs;
