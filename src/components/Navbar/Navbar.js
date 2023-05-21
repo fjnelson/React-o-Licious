83123cc6d9a3c5de66a9313a39674f05849320c8
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-05-20 21:07:34 -0700

    updating icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 //rfce
 import React from 'react'
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+// import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import "./Navbar.css"
